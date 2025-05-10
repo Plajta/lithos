c58c29620cdcac0e9d6b521fdff1a0d6b272bf50
Viktor Suchý <susenky.opavia@gmail.com>
2025-05-10 18:23:01 +0200

    upravovat content path pro github packages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	output: "export",
+	assetPrefix: "/lithos",
+	basePath: "/lithos",
+	trailingSlash: true,
 };
 
 export default nextConfig;
