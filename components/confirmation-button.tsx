a4e9114561264f6e1e390506f6567b62e415b21d
Viktor Suchý <susenky.opavia@gmail.com>
2025-11-20 11:20:22 +0100

    tlacitko na potvrzeni akce

---
+++ components/confirmation-button.tsx
@@ -0,0 +1,36 @@
+import { PopoverClose, PopoverContentProps } from "@radix-ui/react-popover";
+import { Button } from "~/components/ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "~/components/ui/popover";
+
+interface ConfirmationButtonProps {
+	disclaimer: string;
+	action: () => Promise<void>;
+	side?: PopoverContentProps["side"];
+	children: React.ReactNode;
+}
+
+export function ConfirmationButton({ disclaimer, action, side, children }: ConfirmationButtonProps) {
+	return (
+		<Popover>
+			<PopoverTrigger asChild>{children}</PopoverTrigger>
+
+			<PopoverContent className="p-2" side={side}>
+				<div className="flex flex-col gap-2">
+					<p>{disclaimer}</p>
+
+					<div className="flex gap-2">
+						<PopoverClose asChild className="flex-1">
+							<Button variant="outline" size="sm">
+								Zrušit
+							</Button>
+						</PopoverClose>
+
+						<Button variant="destructive" className="flex-1" size="sm" onClick={async () => await action()}>
+							Potvrdit
+						</Button>
+					</div>
+				</div>
+			</PopoverContent>
+		</Popover>
+	);
+}
